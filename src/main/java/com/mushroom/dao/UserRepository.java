4c1d2a7d8f90f992af4bc071b7d70680b1276815
woaiwmm <1946902163@qq.com>
2019-12-20 22:38:53 +0800

    数据访问层

---
+++ src/main/java/com/mushroom/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.mushroom.dao;
+
+import com.mushroom.po.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByUsernameAndPassword(String username, String password);
+}
